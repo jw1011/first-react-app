becacccd18bb5ec9532a550d28e1f7e794ebf08a
Juwon Lee <a8316010@gmail.com>
2023-03-21 17:10:34 +0900

    create box.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import "./App.css";
+import Box from "./component/Box";
 
 function App() {
   return (
     <div>
-      <div className="box">
-        Box 1<p>리사</p>
-      </div>
-      <div className="box">
-        Box 1<p>리사</p>
-      </div>
-      <div className="box">
-        Box 1<p>리사</p>
-      </div>
+      <Box name="리사" num={1} />
+      <Box name="제니" num={2} />
+      <Box name="지수" num={3} />
+      <Box name="로제" num={4} />
     </div>
   );
 }
