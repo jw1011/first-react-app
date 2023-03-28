4332c599e45944c7aac05d4a1eb231b56a1f71b4
Juwon Lee <a8316010@gmail.com>
2023-03-28 21:45:35 +0900

    first commit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import AppClass from "./AppClass";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
